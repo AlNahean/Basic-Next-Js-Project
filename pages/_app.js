2a924eda5c10eaa3224400726e67739ed21719ad
AlNahean <nahean.fardous@gmail.com>
2022-06-15 04:18:58 +0600

    added crud functionality

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+// import '../styles/globals.css'// getting rid of next js basic css
+
+//importing index.scss
+import "../styles/index.scss"
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
